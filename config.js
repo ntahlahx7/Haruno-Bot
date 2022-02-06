dd0298e5336569778d6d3f9ab33c90bd9a5ad521
Shadow master <95863031+ntahlahx7@users.noreply.github.com>
2022-02-06 17:05:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['60189830350', '0','60149431385', '6283175998566', '6285693602003', '6283141445553' ] // Letakan nomor kamu disini
+global.owner = ['6283141445553', '0','6283141445553', '6283141445553', '6283141445553', '6283141445553' ] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
